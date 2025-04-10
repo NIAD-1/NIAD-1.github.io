093c6d61a2ce31aa469a02aabb64639a623f400e
Emmanuel6879 <chinyereenilama@gmail.com>
2025-04-10 08:54:29 +0100

    Hard reset

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 // --- Firebase Configuration ---
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: "AIzaSyCLp8nKO2rb7yMyaL0mM6sJOzoFRmm0BdI",
+    authDomain: "internalaudit-2cd8c.firebaseapp.com",
+    projectId: "internalaudit-2cd8c",
+    storageBucket: "internalaudit-2cd8c.firebasestorage.app",
+    messagingSenderId: "510488169711",
+    appId: "1:510488169711:web:271ed61bd7c4e6c14c5f1a",
+    measurementId: "G-43ESSX8GLJ"
   };
 
   // --- Initialize Firebase ---//
